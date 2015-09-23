4d5c8c76a249a0a8ee02544c69ce628c0a5a873c
Jakub Mrowiec <alkagar@gmail.com>
2015-09-23 08:24:40 +0200

    better consts.

--- const.js
+++ const.js
@@ -1,8 +1,13 @@
+var i = 0;
+var Directions = {
+    'UP': 'UP',
+    'DOWN': 'DOWN',
+    'LEFT': 'LEFT',
+    'RIGHT': 'RIGHT',
+};
+var PossibleDirections = Object.keys(Directions);
+
 module.exports = {
-    Directions: {
-        'UP': 'up',
-        'DOWN': 'down',
-        'LEFT': 'left',
-        'RIGHT': 'right',
-    }
+    Directions: Directions,
+    PossibleDirections: PossibleDirections,
 };
